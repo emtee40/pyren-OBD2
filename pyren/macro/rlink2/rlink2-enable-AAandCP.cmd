fb71960f38d836b99f5ed81f794a59b487fc7659
shrlnm <mshkn@inbox.ru>
2019-05-02 19:36:41 +0300

    9p fix#17

---
+++ pyren/macro/rlink2/rlink2-enable-AAandCP.cmd
@@ -0,0 +1,18 @@
+# Enable AndroidAuto and CarPlay on R-Link2
+$addr = 13
+
+can500  # init can macro
+
+1003
+
+# check if it is rlink2
+exit_if_not F18A 6 4 FFFFFF 434150
+
+set_bits 2130 10 5 60 60
+
+wait 2
+
+# reload rlink2
+1101
+
+exit
---
+++ pyren/macro/rlink2/rlink2-rotate-screen.cmd
@@ -0,0 +1,17 @@
+# Rotate R-Link2 screen
+$addr = 13
+
+can500  # init can macro
+
+1003
+
+exit_if_not F18A 6 4 FFFFFF 434150
+
+xor_bits 2130 10 4 10 10
+
+wait 2
+
+# reload rlink2
+1101
+
+exit
